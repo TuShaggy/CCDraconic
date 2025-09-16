f71ed1c84c99b2268f158a4ae4faf5803c59fd16
TuShaggy <122480537+TuShaggy@users.noreply.github.com>
2025-09-16 16:02:40 +0200

    Update startup.lua

--- startup.lua
+++ startup.lua
@@ -1,6 +1,5 @@
--- startup.lua — arranque mínimo y claro
 if fs.exists("drmon.lua") then
 shell.run("drmon.lua")
 else
-print("drmon.lua no encontrado. Copia los archivos o ejecuta install.")
+print("drmon.lua no encontrado. Ejecuta install.lua para descargar los archivos.")
 end
